75ac4ce5f597a1d2b35c48fa44ba7bc7d8995e79
aashisharcade@gmail.com <aashisharcade@gmail.com>
2019-11-14 12:28:12 +0530

    added object movement

--- game.py
+++ game.py
@@ -1,30 +1,49 @@
 import pygame
 pygame.init()
-scLength = 800
+
+scLength = 1000
 scWidth = 800
+
 win = pygame.display.set_mode((scLength,scWidth))
+pygame.display.set_caption('Pong-Game')
+
+bg = pygame.image.load('bg.png')
+pad = pygame.image.load('paddle.png')
 
 #class for paddle
 class paddle(object):
 
-	def __init__(self, x, y, length, width):
+	def __init__(self, x, y):
 		self.x = x
 		self.y = y
-		self.length = length
-		self.width = width
+		self.vel = 50
 
-	def draw(self,win):
-		pygame.draw.rect(win,(255,255,255),(self.x,self.y,self.length,self.width),9)
+	#display the paddle on screen
+	def draw(self,win,t1,t2):
+		self.move(t1,t2)
+		win.blit(pad,(self.x,self.y))
+
+	#respnsible for movement of paddle
+	def move(self,type1,type2):
+		keys = pygame.key.get_pressed()
+
+		if keys[type1] and self.y - self.vel >= 0:
+			self.y -= self.vel
+		elif keys[type2] and self.y + self.vel + 100 < 800:
+			self.y += self.vel 
+
 
 #as the name suggests
 def redrawGameWindow():
-	P1.draw(win)
-	#P2.draw(win)
+	win.blit(bg,(0,0))
+	P1.draw(win,pygame.K_UP,pygame.K_DOWN)
+	P2.draw(win,pygame.K_w,pygame.K_s)
 	pygame.display.update()
 
 #main-loop
-P1 = paddle(scLength-50,(scWidth-100)//2,10,100)
-#P2 = paddle(40,(scWidth-100)//2,10,100)
+P1 = paddle(scLength-50,(scWidth-180)//2)
+P2 = paddle(30,(scWidth-180)//2)
+
 run = True
 while run:
 	pygame.time.delay(100)
