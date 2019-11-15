5bf9e8095926e68210b0928423849ae6240b8981
aashisharcade@gmail.com <aashisharcade@gmail.com>
2019-11-15 21:32:27 +0530

    created ball class

--- game.py
+++ game.py
@@ -32,17 +32,41 @@ class paddle(object):
 		elif keys[type2] and self.y + self.vel + 100 < 800:
 			self.y += self.vel 
 
+#ball class
+class ball(object):
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+		self.vel = 15
+
+	def draw(self,win):
+		self.move()
+		pygame.draw.circle(win,(255,255,255),(self.x,self.y),15,15)
+
+	def move(self):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_KP8] and self.y - self.vel >= 0:
+			self.y -= self.vel
+		elif keys[pygame.K_KP5] and self.y + self.vel + 100 < 800:
+			self.y += self.vel
+		if keys[pygame.K_KP4] :
+			self.x -= self.vel
+		elif keys[pygame.K_KP6] :
+			self.x += self.vel
 
 #as the name suggests
 def redrawGameWindow():
 	win.blit(bg,(0,0))
 	P1.draw(win,pygame.K_UP,pygame.K_DOWN)
 	P2.draw(win,pygame.K_w,pygame.K_s)
+	B.draw(win)
 	pygame.display.update()
 
 #main-loop
 P1 = paddle(scLength-50,(scWidth-180)//2)
 P2 = paddle(30,(scWidth-180)//2)
+B = ball(30+30,(scWidth-180)//2)
 
 run = True
 while run:
